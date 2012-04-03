1fb95dcfda3471b7c9c39b8f1ba88e0578b20263
Shadab Ahmed <shadab.ansari@gmail.com>
2012-04-03 18:13:54 +0530

    Layout for users login/logout/sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -1,52 +1,58 @@
-Sharedoc::Application.routes.draw
-do
+Sharedoc::Application.routes.draw do
 
-devise_for: users,:path_names = >
-  {
-: sign_in = >'login',: sign_out = >'logout',: password = >'secret',: confirmation = >'verification',: unlock = >'unblock',: registration = >'register',:sign_up =
-      >'cmon_let_me_in'}
-,:controllers = >
-{
-:registrations = >"users/registrations"}
+  devise_for :users, :path => '', :path_names => { :sign_in =>'login', :sign_out =>'logout', :password =>'secret', :confirmation =>'confirmation',
+           :unlock => 'unblock', :registration => 'register',:sign_up => 'sign_up'}, :controllers => { :registrations => 'users/registrations' }
 
-resources: documents root: to = >'documents#index' match 'share/:id' = >'documents#share',: as = >'share_document' match 'view/:id/:conf_id' = >'documents#view',:as =
-  >'view_document'
-#The priority is based upon order of creation:
-#first created -> highest priority.
-#Sample of regular route:
-#match 'products/:id' => 'catalog#view'
-#Keep in mind you can assign values other than :controller and :action
-#Sample of named route:
-#match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-#This route can be invoked with purchase_url(:id => product.id)
-#Sample resource route (maps HTTP verbs to controller actions automatically):
-#resources :products
-#Sample resource route with options:
-#resources :products do
-#member do
-#get 'short'
-#post 'toggle'
-#end
-#
-#collection do
-#get 'sold'
-#end
-#end
-#Sample resource route with sub-resources:
-#resources :products do
-#resources :comments, :sales
-#resource :seller
-#end
-#Sample resource route with more complex sub-resources
-#resources :products do
-#resources :comments
-#resources :sales do
-#get 'recent', :on => :collection
-#end
-#end
-#Sample resource route within a namespace:
-#namespace :admin do
-## Directs /admin/products/* to Admin::ProductsController
+  resources :documents 
+
+  root:to =>'documents#index'
+
+  match 'share/:id' =>'documents#share',:as => 'share_document'
+  match 'view/:id/:conf_id' =>'documents#view',:as => 'view_document'
+
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
+
+  # Sample of regular route:
+  #   match 'products/:id' => 'catalog#view'
+  # Keep in mind you can assign values other than :controller and :action
+
+  # Sample of named route:
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  # This route can be invoked with purchase_url(:id => product.id)
+
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Sample resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Sample resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Sample resource route with more complex sub-resources
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', :on => :collection
+  #     end
+  #   end
+
+  # Sample resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
@@ -60,5 +66,6 @@ resources: documents root: to = >'documents#index' match 'share/:id' = >'documen
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
 end
 
